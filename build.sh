fd2904ad6072324df24433d74c14a46eb7475ea9
agalofreg <andres.galofre@gmail.com>
2026-09-18 21:30:07 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 cd "$(dirname "$0")"
 {
   cat src/00-shell-inicio.html src/10-inicio.html src/20-tema.html src/30-detalle.html src/40-formacion.html src/50-pieza.html src/60-guardados.html src/70-perfil.html src/75-registro.html src/80-shell-cierre.html src/85-historia.html src/90-fin.html
-  printf '\n<script src="src/app.js"></script>\n</body>\n</html>\n'
+  printf '\n<script src="src/app.js?v=%s"></script>\n</body>\n</html>\n' "$(date +%Y%m%d%H%M)"
 } > index.html
 echo "index.html generado"
